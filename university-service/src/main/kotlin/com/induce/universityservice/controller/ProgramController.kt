dc47c4bb5ab3e9388667ab07e119553c1f6d3937
Induce <aa5619721@gmail.com>
2026-04-19 18:53:54 +0300

    update university-service

--- university-service/src/main/kotlin/com/induce/universityservice/controller/ProgramController.kt
+++ university-service/src/main/kotlin/com/induce/universityservice/controller/ProgramController.kt
@@ -1,21 +1,32 @@
 package com.induce.universityservice.controller
 
+import com.induce.universityservice.dto.CreateProgramRequest
 import com.induce.universityservice.dto.ProgramResponse
 import com.induce.universityservice.service.ProgramService
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/university/programs")
 class ProgramController(
-    private val service: ProgramService
+    private val programService: ProgramService
 ) {
     @GetMapping("/{id}")
     fun getProgram(
         @PathVariable id: Long
     ): ProgramResponse {
-        return service.getProgram(id)
+        return programService.getProgram(id)
+    }
+
+    @PostMapping("/faculty/{facultyId}")
+    fun createProgram(
+        @PathVariable facultyId: Long,
+        @RequestBody request: CreateProgramRequest
+    ): ProgramResponse {
+        return programService.createProgram(facultyId, request)
     }
 }
